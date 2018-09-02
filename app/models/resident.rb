73a7074386b7f2eeabd293aa2839e4da5ac083cd
Aline <alinemokfa@hotmail.co.uk>
2018-09-02 21:18:56 +0100

    Change belongs_to relation to optional

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,3 +1,4 @@
 class Resident < ApplicationRecord
-  belongs_to :room
+  # In Rails 5, belongs_to is required by default. By adding optional: true, a resident can be added even if a room has not been assigned to them.
+  belongs_to :room, optional: true
 end
