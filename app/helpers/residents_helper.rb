5a9f35ec0cfd4fc5b6dd219ef740671973ad5b12
Aline <alinemokfa@hotmail.co.uk>
2018-09-04 09:33:42 +0100

    Moved resident logic from views to helper

--- app/helpers/residents_helper.rb
+++ app/helpers/residents_helper.rb
@@ -1,2 +1,21 @@
 module ResidentsHelper
+  def shared_room?(resident)
+    if resident.shared_room
+      'Yes'
+    else
+      'No'
+    end
+  end
+
+  def assigned_room?(resident)
+    if resident.room
+      "#{resident.room.number}"
+    else
+      'Not assigned'
+    end
+  end
+
+  def select_options(rooms)
+    rooms.map { |f| f.number }
+  end
 end
