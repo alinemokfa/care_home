24be037dbe793ce763710c2b106ac7ebd315fe46
Aline <alinemokfa@hotmail.co.uk>
2018-09-05 13:20:19 +0100

    Add capacity validation comments

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,18 @@
 module RoomsHelper
+  # TODO At the moment, assigning a room to a resident does not take into consideration room.capacity. Validation needed.
+  #      Create function that does the incrementing and updates the db (not sure how to do so w/ Rails - research needed!)
+  #      The room should have a residents variable in it as it needs to know both its capacity and how many residents are in it.
+  #
+  # def add_resident(room)
+  #     if room.has_capacity()
+  #         room.residents += 1
+  #       UPDATE DB?
+  #     else
+  #         "Sorry, this rooms is full"
+  #    end
+  # end
+  #
+  # def has_capacity(room)
+  #     room.residents < room.capacity
+  # end
 end
